bea4b4152e6047823932e7030dc71abe88728be0
rusakovr <rusakovr@mail.ru>
2016-03-15 19:10:24 +0300

    First Commit

---
+++ SigCrt/Element.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace SigCrt
+{
+    class Element
+    {
+        private string name;
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        private double elementValue;
+        public double Value
+         {
+            set
+            {
+                elementValue = value;
+            }
+            get
+            {
+                return elementValue;
+            }
+        }
+        private string type;
+        public string Type
+        {
+            set
+            {
+                Regex regex = new Regex("R|C|OpAmp");
+                if (regex.IsMatch(value))
+                {
+                    type = value;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong type name in netlist");
+                }
+            }
+            get
+            {
+                return type;
+            }
+        }
+    }
+}
---
+++ SigCrt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SigCrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Element el = new Element();
+            el.Type = "x";
+        }
+    }
+}
